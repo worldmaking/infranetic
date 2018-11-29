95b1b427bce97271dcc84c4aaa088651c9e2754f
Graham Wakefield <grrrwaaa@gmail.com>
2018-11-28 22:33:38 -0500

    last

--- launch_infranet.bat
+++ launch_infranet.bat
@@ -1,5 +1,7 @@
 :restart
 
+start "" "audio\audio.maxpat"
+
 taskkill /F /IM chrome* /T
 start "" "chrome" --user-data-dir=c:\temp1 --window-position=1920,0 --start-fullscreen --app="http://localhost:8080/grid.html?mouse=off" 
 start "" "chrome" --user-data-dir=c:\temp2 --window-position=0,0 --start-fullscreen --app="http://localhost:8080/main.html?mouse=off"
@@ -7,6 +9,7 @@ start "" "chrome" --user-data-dir=c:\temp2 --window-position=0,0 --start-fullscr
 node server.js
 
 taskkill /F /IM chrome* /T
+taskkill /F /IM max* /T
 timeout 2 > nul
 
 rem goto restart
--- server.js
+++ server.js
@@ -28,8 +28,26 @@ console.log("project_path", project_path);
 console.log("server_path", server_path);
 console.log("client_path", client_path);
 
-
-
+// let ctrl-c quit:
+{
+	let stdin = process.stdin;
+	// without this, we would only get streams once enter is pressed
+	stdin.setRawMode( true );
+	// resume stdin in the parent process (node app won't quit all by itself
+	// unless an error or process.exit() happens)
+	stdin.resume();
+	// i don't want binary, do you?
+	stdin.setEncoding( 'utf8' );
+	// on any data into stdin
+	stdin.on( 'data', function( key ){
+	// ctrl-c ( end of text )
+	if ( key === '\u0003' ) {
+		process.exit();
+	}
+	// write the key to stdout all normal like
+	process.stdout.write( key );
+	});
+}
 
 // loads an image and turns it into a typedarray and offscreen canvas
 class ArrayFromImg {
